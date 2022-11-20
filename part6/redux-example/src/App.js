e7439c3693942dbcb6ab515ffbe19d2aa47833f3
Oleksandr Pylypenko <lex.pilipenko@gmail.com>
2022-11-20 23:12:29 +0200

    complete b block

--- part6/redux-example/src/App.js
+++ part6/redux-example/src/App.js
@@ -1,14 +1,26 @@
-import NewNote from "./components/NewNote"
-import Notes from "./components/Notes"
+import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value)
+  }
 
   return (
     <div>
       <NewNote />
+      <div>
+        <input type="radio" name="filter"
+          onChange={() => filterSelected('ALL')} />all
+        <input type="radio" name="filter"
+          onChange={() => filterSelected('IMPORTANT')} />important
+        <input type="radio" name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')} />nonimportant
+      </div>
       <Notes />
     </div>
   )
 }
+
 
 export default App
